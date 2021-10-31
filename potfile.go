623670a9be23b4b07263a2771badad7766ff9556
Nick Yakovliev <mail@mkla.dev>
2021-10-30 21:45:34 -0500

    Include salt in potfile and ensure it respects `--hex-salt` arg

--- potfile.go
+++ potfile.go
@@ -6,6 +6,28 @@ import (
 	"os"
 )
 
+func stringToHex(s string) string {
+	var result string
+	for _, c := range s {
+		result += fmt.Sprintf("%02x", c)
+	}
+	return result
+}
+
+func writeToPotfile(settings *Settings, potfile *os.File, task Task) error {
+	var err error
+	if len(task.salt) > 0 {
+		salt := task.salt
+		if *settings.hexSalt {
+			salt = stringToHex(task.salt)
+		}
+		_, err = potfile.WriteString(fmt.Sprintf("%s:%s:%s\n", task.hash, salt, task.word))
+	} else {
+		_, err = potfile.WriteString(fmt.Sprintf("%s:%s\n", task.hash, task.word))
+	}
+	return err
+}
+
 func potfileWriter(settings *Settings) {
 	potfile, err := os.OpenFile(*settings.potfile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.FileMode(777))
 	if err != nil {
@@ -14,7 +36,7 @@ func potfileWriter(settings *Settings) {
 	for {
 		select {
 		case task := <-*settings.results:
-			_, err := potfile.WriteString(fmt.Sprintf("%s:%s\n", task.hash, task.word))
+			err := writeToPotfile(settings, potfile, task)
 			settings.writes.Done()
 			if err != nil {
 				panic(err)
